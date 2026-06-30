a33dc3bdcbe9cade1f648992482dca31a212577b
ZÉ (Vanessa) <vanessa.naeser@escola.pr.gov.br>
2026-06-30 19:03:42 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+const botoes = document.querySelectorAll("button");
+
+
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado);
+    function botaoClicado() {
+        console.log("botão clicado");
+        let texto = botao.querySelector("span");
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+        } else {
+            texto.textContent--;
+            curtiu = false;
+        }
+    }
+});
